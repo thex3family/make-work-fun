917c5fc4965b46477ecf588fcca96fa81f5c4796
Conrad <git@conradlin.com>
2021-06-18 16:41:41 +0800

    truncate text for better readability

--- components/Cards/CardAvatar.js
+++ components/Cards/CardAvatar.js
@@ -93,7 +93,7 @@ export default function Avatar({
               <h5 className="text-emerald-400 uppercase font-bold text-xs">
                 {statTitle}
               </h5>
-              <p className="font-semibold text-xl text-white-700">
+              <p className="font-semibold text-xl text-white-700 truncate">
                 {statName}
               </p>
               <span className="font-semibold text-l text-white-700">
@@ -153,7 +153,7 @@ export default function Avatar({
       <div className="">
         <div className="">
           <p className="text-white text-md font-semibold w-3/5">Latest Win 👀</p>
-          <p className="text-white text-sm">{statWinName}</p>
+          <p className="text-white text-sm truncate">{statWinName}</p>
           <p className="text-xs mt-3 text-right"><span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-lightBlue-700 bg-lightBlue-200 uppercase last:mr-0 mr-1">{statWinType}</span><span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-yellow-600 bg-yellow-200 uppercase last:mr-0 mr-1">+{statWinGold} 💰 </span><span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-emerald-600 bg-emerald-200 uppercase last:mr-0 mr-1">+{statWinEXP}XP</span></p>
         </div>
       </div>
