ad231f53fe6dfb608a555fcb63db205a3ff4c649
Joshwin Greene <joshwin.greene@gmail.com>
2021-07-25 23:40:39 -0700

    Created the Edit Dailies page using that Ant Design table. Doing the following operations locally works: add habit, edit name or status of habit, and delete habit. The next step is to update the backend when the aforementioned operations take place.

---
+++ pages/dailies/edit.js
@@ -0,0 +1,282 @@
+import Link from 'next/link';
+//import Button from '@/components/ui/Button';
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import { supabase } from '../../utils/supabase-client';
+import { useUser } from '@/utils/useUser';
+import { useRouter } from 'next/router';
+import BottomNavbar from '@/components/ui/BottomNavbar/BottomNavbar';
+import { Table, Input, Button, Popconfirm, Form } from 'antd';
+import 'antd/dist/antd.css';
+const EditableContext = React.createContext(null);
+
+function generateTable(dataSource, columns, EditableRowFunc, EditableCellFunc, handleAddFunc, handleSaveFunc) {
+    //console.log("Datasource: ", dataSource);
+    //console.log("Columns: ", columns);
+
+    const components = {
+        body: {
+          row: EditableRowFunc,
+          cell: EditableCellFunc,
+        },
+    };
+    
+    const cols = columns.map((col) => {
+        if (!col.editable) {
+            return col;
+        }
+  
+        return {
+          ...col,
+          onCell: (record) => ({
+            record,
+            editable: col.editable,
+            dataIndex: col.dataIndex,
+            title: col.title,
+            handleSave: handleSaveFunc,
+          }),
+        };
+    });
+
+      return (
+        <div>
+          <Button
+            onClick={handleAddFunc}
+            type="primary"
+            style={{
+              marginBottom: 16,
+            }}
+          >
+            Add Habit
+          </Button>
+          <Table
+            components={components}
+            rowClassName={() => 'editable-row'}
+            bordered
+            dataSource={dataSource}
+            columns={cols}
+          />
+        </div>
+      );
+}
+
+function EditableRow ({ index, ...props }) {
+    const [form] = Form.useForm();
+    return (
+      <Form form={form} component={false}>
+        <EditableContext.Provider value={form}>
+          <tr {...props} />
+        </EditableContext.Provider>
+      </Form>
+    );
+  };
+  
+  function EditableCell ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+  }) {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef(null);
+    const form = useContext(EditableContext);
+    useEffect(() => {
+      if (editing) {
+        inputRef.current.focus();
+      }
+    }, [editing]);
+  
+    const toggleEdit = () => {
+      console.log("record: ", record);
+      setEditing(!editing);
+      form.setFieldsValue({
+        [dataIndex]: record[dataIndex],
+      });
+    };
+  
+    const save = async () => {
+      try {
+        const values = await form.validateFields();
+        toggleEdit();
+        console.log("save - values: ", values);
+        if (values.is_active) {
+          handleSave({ ...record, is_active: values.is_active === "true" });
+        } else {
+          handleSave({ ...record, ...values });
+        }
+      } catch (errInfo) {
+        console.log('Save failed:', errInfo);
+      }
+    };
+  
+    let childNode = children;
+  
+    if (editable) {
+      childNode = editing ? (
+        <Form.Item
+          style={{
+            margin: 0,
+          }}
+          name={dataIndex}
+          rules={[
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ]}
+        >
+          <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+        </Form.Item>
+      ) : (
+        <div
+          className="editable-cell-value-wrap"
+          style={{
+            paddingRight: 24,
+          }}
+          onClick={toggleEdit}
+        >
+          {children}
+        </div>
+      );
+    }
+  
+    return <td {...restProps}>{childNode}</td>;
+  };
+
+export default function edit() {
+    const [habits, setHabits] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const {
+      userLoaded,
+      user,
+      session,
+      userDetails,
+      userOnboarding,
+      subscription
+    } = useUser();
+
+    const columns = [
+      {
+        title: 'name',
+        dataIndex: 'name',
+        width: '30%',
+        editable: true,
+      },
+      {
+        title: 'status',
+        dataIndex: 'is_active',
+        editable: true,
+        render: is_active => (
+          <div>{ is_active ? "Active" : "Inactive" }</div>
+        )
+      },
+      {
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (_, record) =>
+          habits.length >= 1 ? (
+            <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.id)}>
+              <a>Delete</a>
+            </Popconfirm>
+          ) : null,
+      },
+    ];
+  
+    useEffect(() => {
+      if (userOnboarding) initializePlayer();
+    }, [userOnboarding]);
+  
+    function initializePlayer() {
+      try {
+        if (userOnboarding.onboarding_state.includes('4')) {
+          loadPlayer();
+        } else {
+          router.replace('/account');
+        }
+      } catch (error) {
+        alert(error.message);
+      } finally {
+        console.log('InitializedPlayer');
+      }
+    }
+  
+    // If player is ready to load, go for it!
+  
+    async function loadPlayer() {
+      console.log('Loading Player');
+      fetchHabits();
+    }
+  
+    async function fetchHabits() {
+      try {
+        const user = supabase.auth.user();
+  
+        const { data, error } = await supabase
+          .from('habits')
+          .select('*')
+          .eq('player', user.id);
+  
+        setHabits(data);
+
+        if (error && status !== 406) {
+          throw error;
+        }
+      } catch (error) {
+        // alert(error.message)
+      } finally {
+        setLoading(false);
+        console.log("fetchHabits - finally - ", habits);
+      }
+    }
+
+    function handleDelete(id) {
+        const dataSource = [...habits];
+        setHabits(dataSource.filter((item) => item.id !== id));
+    }
+
+    function handleAdd() {
+        const newData = {
+            key: habits.length,
+            name: "New Habit",
+            is_active: true,
+        };
+        setHabits([...habits, newData]);
+    }
+
+    function handleSave(row) {
+        const newData = [...habits];
+        const index = newData.findIndex((item) => row.id === item.id);
+        const item = newData[index];
+        newData.splice(index, 1, { ...item, ...row });
+        setHabits(newData);
+    }
+
+    return (
+        <section className="justify-center bg-dailies-pattern bg-fixed">
+    <BottomNavbar/>
+      <div className=" max-w-6xl mx-auto py-8 sm:pt-24 px-4 sm:px-6 lg:px-8 my-auto w-full flex flex-col">
+        <div className="bg-dailies-default rounded p-10 opacity-95">
+          <div className="pb-5">
+            <h1 className="text-4xl font-extrabold text-center sm:text-6xl text-dailies pb-5">
+              Edit Dailies
+            </h1>
+          </div>
+          <div className="text-center my-5">
+            <Link href="/dailies">
+              <button
+                className="px-5 border-2 border-dailies-dark text-center text-dailies font-bold py-2 rounded"
+              >
+                Back
+              </button>
+            </Link>
+          </div>
+          <div className="text-center my-5">
+            { habits ? generateTable(habits, columns, EditableRow, EditableCell, handleAdd, handleSave) : null }
+          </div>
+        </div>
+      </div>
+    </section>
+    );
+}
