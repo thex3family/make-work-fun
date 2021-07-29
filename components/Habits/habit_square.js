d5aae26de35bd73eb10bec6adb3b1dd45bb4fc48
Conrad <git@conradlin.com>
2021-07-29 22:48:41 +0800

    Add saving badge per card

--- components/Habits/habit_square.js
+++ components/Habits/habit_square.js
@@ -128,36 +128,37 @@ export default function HabitSquare({
       onClick={
         saving ? null : () => handleHabitCompletionStatusChange(habit_id)
       }
-      className={`my-4 mb-0 sm:mb-8 p-4 sm:p-6 w-full sm:w-64 ${
+      className={`my-4 mb-0 sm:mb-8 p-4 sm:p-6 w-full sm:w-64 relative ${
         habitCompletedToday
           ? `bg-emerald-500 border-emerald-700`
           : `bg-dailies-light border-dailies-dark`
       } rounded z-10 square cursor-pointer shadow-lg border-4`}
     >
+      
       {saving ? (
-        <span className="fixed inline-flex left-0 bottom-0 ml-2 mb-24 sm:ml-4 sm:mb-4 text-md font-semibold py-3 px-4 uppercase rounded text-emerald-600 bg-emerald-200 z-50">
-          <svg
-            className="animate-spin -ml-1 mr-3 h-5 w-5 text-emerald-600"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              stroke-width="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-          Saving...
-        </span>
+        <div className="inline-flex absolute top-0 right-0 mt-2 mr-2 text-xs font-semibold py-2 px-3 uppercase rounded text-white bg-gradient-to-r from-emerald-500 to-blue-500 border-emerald-500 z-50">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-4 w-4 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+        Saving...
+      </div>
       ) : (
         <div></div>
       )}
