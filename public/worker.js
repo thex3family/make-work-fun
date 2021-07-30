1f5787051f102fbe30cd49575ac348f3a29e7523
Conrad <git@conradlin.com>
2021-07-30 14:51:15 +0800

    Testing Notification

--- public/worker.js
+++ public/worker.js
@@ -2,7 +2,6 @@ self.__WB_DISABLE_DEV_LOGS = true
 
 self.addEventListener('notificationclick', function(e) {
     var notification = e.notification;
-    var primaryKey = notification.data.primaryKey;
     var action = e.action;
   
     if (action === 'close') {
