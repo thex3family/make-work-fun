e96a22d30fbd097c90618128e9dcd1ac0e3a75b3
Joshwin Greene <joshwin.greene@gmail.com>
2021-08-13 07:05:15 -0700

    Increased the text size of the sub headers + Added a progress bar

--- pages/parties.js
+++ pages/parties.js
@@ -15,7 +15,7 @@ export default function parties() {
           </div>
           <div className="text-center">
             <section>
-              <h2 className="text-4x1 font-bold text-dailies">Your Active Party</h2>
+              <h2 className="text-xl align-middle justify-center inline-flex font-bold text-dailies">Your Active Party</h2>
               <div className="max-w-lg bg-white shadow-md rounded-lg overflow-hidden mx-auto mt-2">
                 <div className="py-4 px-8 mt-3">
                   <div className="flex flex-col mb-8">
@@ -28,7 +28,11 @@ export default function parties() {
                     <p className="text-black">
                       memberAvatar1 memberAvatar2 memberAvatar3
                     </p>
-                    <p className="text-black">[progress bar]</p>
+                    <div className="w-full">
+                      <div className="shadow w-full bg-grey-light mt-2">
+                        <div className="bg-green text-xs leading-none py-1 text-center text-white w-3/4 h-4"></div>
+                      </div>
+                    </div>
                     <p className="text-black">Metrics</p>
                   </div>
                   <div className="mt-1">
@@ -43,7 +47,7 @@ export default function parties() {
               </div>
             </section>
             <section>
-              <h2 className="text-4x1 font-bold text-dailies mt-4">Parties Recruiting</h2>
+              <h2 className="text-xl align-middle justify-center inline-flex font-bold text-dailies mt-4">Parties Recruiting</h2>
             </section>
           </div>
         </div>
