70638e40faa73e481eb049f1c2b5d9a84d4fb21b
Conrad <git@conradlin.com>
2021-08-27 17:36:09 +0800

    Count Up

--- components/Widgets/Statistics/LeaderboardStatistics.js
+++ components/Widgets/Statistics/LeaderboardStatistics.js
@@ -1,54 +1,63 @@
 import s from './LeaderboardStatistics.module.css';
+import CountUp from 'react-countup';
 
 export default function LeaderboardStatistics({
   players,
   wins_earned,
   exp_earned,
   levels_earned,
-  gold_earned,
+  gold_earned
 }) {
+  return (
+    //   <span className={s.Countdowncol}>
+    //     <span className={s.Countdowncolelement}>
+    //         <strong>{wins_earned}</strong>
+    //         <span className={s.Countdowncolformat}>{wins_earned === 1 ? 'Win' : 'Wins'}</span>
+    //     </span>
+    //   </span>
 
-    return (
-
-      //   <span className={s.Countdowncol}>
-      //     <span className={s.Countdowncolelement}>
-      //         <strong>{wins_earned}</strong>
-      //         <span className={s.Countdowncolformat}>{wins_earned === 1 ? 'Win' : 'Wins'}</span>
-      //     </span>
-      //   </span>
-
-      <div className={`${s.Countdown} overflow-x-auto`}>
-      
+    <div className={`${s.Countdown} overflow-x-auto`}>
       <span className={s.Countdowncol}>
         <span className={s.Countdowncolelement}>
-          <strong>{players} <i className="fas fa-running"/></strong>
-          <span className={s.Countdowncolformat}>{players === 1 ? 'Player' : 'Players'}</span>
+          <strong>
+            <CountUp start={0} end={players} duration={1} separator="," />{' '}
+            <i className="fas fa-running" />
+          </strong>
+          <div className={`${s.Countdowncolformat} bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-blue-500`}>
+            {players === 1 ? 'Player' : 'Players'}
+          </div>
         </span>
       </span>
-      
-        <span className={s.Countdowncol}>
-          <span className={s.Countdowncolelement}>
-            <strong>{levels_earned} <i className="fas fa-angle-double-up"/></strong>
-            <span className={s.Countdowncolformat}>{levels_earned === 1 ? 'Level Up' : 'Level Ups'}</span>
-          </span>
+
+      <span className={s.Countdowncol}>
+        <span className={s.Countdowncolelement}>
+          <strong>
+            <CountUp start={0} end={levels_earned} duration={1} separator="," />{' '}
+            <i className="fas fa-angle-double-up" />
+          </strong>
+          <div className={`${s.Countdowncolformat} bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-blue-500`}>
+            {levels_earned === 1 ? 'Level Up' : 'Level Ups'}
+          </div>
         </span>
+      </span>
 
-        <span className={s.Countdowncol}>
-          <span className={s.Countdowncolelement}>
-            <strong>{exp_earned} XP</strong>
-            <span className={s.Countdowncolformat}>EXP Earned</span>
-          </span>
+      <span className={s.Countdowncol}>
+        <span className={s.Countdowncolelement}>
+          <strong>
+            <CountUp start={0} end={exp_earned} duration={1} separator="," />{' '}
+            XP
+          </strong>
+          <div className={`${s.Countdowncolformat} bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-blue-500`}>EXP Earned</div>
         </span>
+      </span>
 
-
-{/* 
+      {/* 
         <span className={s.Countdowncol}>
           <span className={s.Countdowncolelement}>
             <strong>{gold_earned}</strong>
             <span className={s.Countdowncolformat}>Gold</span>
           </span>
         </span> */}
-
-      </div>
-    );
+    </div>
+  );
 }
