4cb860a2f383de7cfe6f69753eb58634b95937d3
bencovington-game <56565566+bencovington-game@users.noreply.github.com>
2021-08-27 19:15:30 -0500

    Update Kanban.js

--- components/Parties/Kanban.js
+++ components/Parties/Kanban.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { useReducer } from 'react';
+import { supabase } from '@/utils/supabase-client';
+import Button from "@/components/ui/Button"
+
+
 const imgs = [
     "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.25&w=144&h=144&q=80",
     "https://images.unsplash.com/photo-1526510747491-58f928ec870f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.25&w=144&h=144&q=80",
     "https://images.unsplash.com/photo-1506697084665-7f7d652308af?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.25&w=144&h=144&q=80",
 ];
 const style = {
-    container: `w-full h-screen p-2 flex-1 bg-gray-800`,
-    kanban: `w-full h-full inline-flex overflow-hidden space-x-2`,
-    column: `w-80 h-full flex-col`,
+    container: `w-full h-screen p-2 flex-1 bg-dailies-default`,
+    kanban: `w-full h-full grid-cols-2 inline-flex overflow-hidden space-x-2`,
+    column: `w-full h-full flex-col text-dailies`,
     deck: `h-full overflow-auto`,
-    card: `bg-gray-500 rounded-lg p-2 m-2 shadow-xl`,
-    members : `mt-1 flex items-center`,
-    image: (s) => `w-6 h-6 rounded-full object-cover border-2 border-gray-500 ${s}`
+    card: `bg-white rounded-lg p-2 m-2 shadow-xl text-black`,
+    members : `mt-1 flex items-center w-10`,
+    image: (s) => `w-6 h-6 rounded-full object-cover border-2 border-white ${s}`
 };
 const description = `Long sentence too serve as the description goes here`
 const info = [
@@ -39,7 +44,51 @@ const clickHandler = (e) => {
     const parent = e.target.parentElement;
     let target = isButton(parent) ? parent :
     isButton(parent.parentElement) ? parent.parentElement: e.target;
+    console.log(`clicked`);
 };
+
+
+function reducer(state, action) {
+    switch(action.type){
+        case "start" : 
+            console.log(`starting`);
+            break;
+        default:
+            console.log(`default`);
+            break;
+    };
+};
+
+
+///////////////////////////////////////////////////////////
+
+async function fetchRecruitingParties() {
+    try {
+      // get the parties that the user is a part of
+      const { data, error } = await supabase
+        .from('party')
+        .select('*')
+        .eq('status', 1);
+
+      if (data) {
+        console.log('Recruiting Parties', data);
+        return data;
+      }
+
+      if (error && status !== 406) {
+        throw error;
+      }
+    } 
+    catch (error) {
+      alert(error.message)
+    } 
+    // finally {
+    //   setLoading(false);
+    // }
+  }
+//   const parties = await fetchRecruitingParties();
+
+///////////////////////////////////////////////////////////
 const Members = props =>           
     <div className={style.members}>
     {props.memberList.map((img,i)=> <img
@@ -51,19 +100,30 @@ const Members = props =>
     </div>
 const Entry = props => 
     <li className={style.card}>
-        <button className={`w-full`} onClick={clickHandler}>
-            <h3>
-                {props.title || `Title`}
-            </h3>
-            <p className="text-left">
-                {props.description || `Description`}
-            </p>
-            <Members memberList={props.members}></Members>
-        </button>
+        <h3>
+            {props.title || `Title`}
+        </h3>
+        <p className="text-left">
+            {props.description || `Description`}
+        </p>
+        <div className={`flex justify-between mt-2`}>
+            <Members 
+            className={`ml-5`} 
+            memberList={props.members}></Members>
+            <Button
+                variant="prominent"
+                disabled={false}
+                className="animate-fade-in-up text-center font-bold"
+                onClick={clickHandler}>
+                Join Up!
+            </Button>
+        </div>
     </li>
 const Column = props => 
     <div className={style.column}>
-        <div><h3 className="ml-3">{props.title}</h3></div>
+        <div><h3 className="text-center font-bold">
+            {props.title}
+        </h3></div>
         <div className="h-full">
             <ul className={style.deck}>
                 {Object.entries(props.entries).map(([_,entry],i,array)=>
@@ -73,6 +133,11 @@ const Column = props =>
         </div>
     </div>
 const Kanban = props => {
+    const initialParties = fetchRecruitingParties();
+    const [parties, dispatch] = useReducer(reducer, initialParties);
+    // console.log(initialParties);
+    // dispatch({type:"start"})
+    console.log(parties);
     return (
     <div name="kanban_container" className={style.container}>
         <div className={style.kanban}>
