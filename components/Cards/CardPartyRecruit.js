6e96b5551dfc0544c1bfa0d11dda21f3f8dfc193
Conrad <git@conradlin.com>
2021-09-01 20:55:06 +0800

    Add Members

--- components/Cards/CardPartyRecruit.js
+++ components/Cards/CardPartyRecruit.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Button from '@/components/ui/Button';
+import { useState, useEffect } from 'react';
+import { fetchPartyMembers } from '../Fetch/fetchMaster';
 
 export default function CardPartyRecruit({party}) {
+  
+  const [partyMembers, setPartyMembers] = useState(null);
+
+  useEffect(() => {
+    getPartyMembers();
+  }, []);
+
+  async function getPartyMembers(){
+    setPartyMembers(await fetchPartyMembers(party.id));
+  }
+
   const style = {
     card: `bg-yellow-50 shadow-lg w-56 sm:w-72 m-2 p-4 text-dailies rounded-lg flex flex-col gap-1 justify-center`,
-    name: `w-50 font-bold text-lg`,
+    name: `w-50 font-bold text-xl`,
     playerIcons: `w-8 h-8 m-1 border-2 rounded-full`,
     leader: `border-indigo-300 ml-0`,
     member: `border-yellow-100 -ml-3`,
@@ -21,16 +34,44 @@ export default function CardPartyRecruit({party}) {
         {party.description}
         </div>
         <div className="mt-auto">
-          <div className="flex justify-center mb-2">
-            {playersExample.map((player, i) => (
+          {/* <div className="flex justify-center mb-2">
+            {members.map((member, i) => (
               <img
-                src={player}
+                src={member.avatar_url}
                 className={`${style.playerIcons} ${
                   i > 0 ? style.member : style.leader
                 }`}
               ></img>
             ))}
-          </div>
+          </div> */}
+          <div className="row-start-2 justify-center hidden sm:flex mb-3">
+                {partyMembers
+                  ? partyMembers.map((member) => (
+                      <div
+                        className={`bg-cover bg-center object-cover rounded-full shadow-xl block w-8 h-8 -ml-3 overflow-hidden ${
+                          member.role == 'Party Leader'
+                            ? 'border-2 border-yellow-300'
+                            : 'border-2 border-gray-700'
+                        }`}
+                        style={{
+                          backgroundImage: `url(${member.background_url})`
+                        }}
+                      >
+                        <div className="bg-black bg-opacity-30 rounded-full w-8 h-8 p-0.5 flex items-center">
+                          <img
+                            className="avatar image mx-auto object-cover"
+                            src={`${
+                              member.avatar_url
+                                ? member.avatar_url
+                                : 'img/default_avatar.png'
+                            }`}
+                            alt="Avatar"
+                          />
+                        </div>
+                      </div>
+                    ))
+                  : null}
+              </div>
             <Button
               variant="prominent"
               disabled={false}
