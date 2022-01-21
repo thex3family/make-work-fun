914229e6c30ded8ae66318174de90238177287ae
Conrad Lin <git@conradlin.com>
2022-01-21 17:25:28 +0800

    Clean up layout

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,6 @@ import { useUser } from '@/utils/useUser';
 
 
 export default function Layout({ children, meta: pageMeta }) {
-  const router = useRouter();
   const meta = {
     title: 'Make Work Fun, Get Stuff Done',
     description:
@@ -18,6 +17,7 @@ export default function Layout({ children, meta: pageMeta }) {
     cardImage: '/og.png',
     ...pageMeta
   };
+  const router = useRouter();
 
   const { user, userProfile, userOnboarding } = useUser();
 
@@ -31,58 +31,73 @@ export default function Layout({ children, meta: pageMeta }) {
     console.log(userProfile?.full_name)
   }, [user]);
 
-  
-function setupIntercom(mobileDevice) {
-  // hide it on embeds until I figure out a good way to pass user credentials to intercom
-  if (process.env.NODE_ENV === 'production' && !router.asPath.includes('embed/')) {
-    window.intercomSettings = {
-      app_id: 'dcx9wsn6',
-      hide_default_launcher: mobileDevice,
-      email: user?.email,
-      name: userProfile?.full_name,
-      custom_launcher_selector:'.launch_intercom'
-    };
 
-    (function () {
-      var w = window;
-      var ic = w.Intercom;
-      if (typeof ic === 'function') {
-        ic('reattach_activator');
-        ic('update', w.intercomSettings);
-      } else {
-        var d = document;
-        var i = function () {
-          i.c(arguments);
-        };
-        i.q = [];
-        i.c = function (args) {
-          i.q.push(args);
-        };
-        w.Intercom = i;
-        var l = function () {
-          var s = d.createElement('script');
-          s.type = 'text/javascript';
-          s.async = true;
-          s.src = 'https://widget.intercom.io/widget/dcx9wsn6';
-          var x = d.getElementsByTagName('script')[0];
-          x.parentNode.insertBefore(s, x);
-        };
-        if (w.attachEvent) {
-          w.attachEvent('onload', l);
+  function setupIntercom(mobileDevice) {
+    // hide it on embeds until I figure out a good way to pass user credentials to intercom
+    if (process.env.NODE_ENV === 'production' && !router.asPath.includes('embed/')) {
+      window.intercomSettings = {
+        app_id: 'dcx9wsn6',
+        hide_default_launcher: mobileDevice,
+        email: user?.email,
+        name: userProfile?.full_name,
+        custom_launcher_selector: '.launch_intercom'
+      };
+
+      (function () {
+        var w = window;
+        var ic = w.Intercom;
+        if (typeof ic === 'function') {
+          ic('reattach_activator');
+          ic('update', w.intercomSettings);
         } else {
-          w.addEventListener('load', l, false);
+          var d = document;
+          var i = function () {
+            i.c(arguments);
+          };
+          i.q = [];
+          i.c = function (args) {
+            i.q.push(args);
+          };
+          w.Intercom = i;
+          var l = function () {
+            var s = d.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = 'https://widget.intercom.io/widget/dcx9wsn6';
+            var x = d.getElementsByTagName('script')[0];
+            x.parentNode.insertBefore(s, x);
+          };
+          if (w.attachEvent) {
+            w.attachEvent('onload', l);
+          } else {
+            w.addEventListener('load', l, false);
+          }
         }
-      }
-    })();
-    console.log('Intercom Started');
+      })();
+      console.log('Intercom Started');
+    }
   }
-}
 
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
         <title>{meta.title}</title>
-        <meta name="robots" content="follow, index" />
         <link href="/favicon.ico" rel="shortcut icon" />
         <meta content={meta.description} name="description" />
         <meta
@@ -104,11 +119,11 @@ function setupIntercom(mobileDevice) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
-      {!router.asPath.includes('embed/') && !router.asPath.includes('signin') && !router.asPath.includes('auth')   ? <Navbar /> : null}
+      {!router.asPath.includes('embed/') && !router.asPath.includes('signin') && !router.asPath.includes('auth') ? <Navbar /> : null}
       <main id="skip">{children}</main>
       {userOnboarding ? (
         userOnboarding.onboarding_state.includes('4') &&
-        !router.asPath.includes('embed/') ? (
+          !router.asPath.includes('embed/') ? (
           <BottomNavbar />
         ) : null
       ) : null}
--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,7 @@ import Head from 'next/head';
 //       hide_default_launcher: mobileDevice,
 //       email: "bob@example.com",
 //     };
-  
+
 //     (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/dcx9wsn6';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 //     console.log('Intercom Started');
 //   }
@@ -41,24 +41,6 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
-      </Head>
       <div className="bg-primary">
         <UserContextProvider>
           <Layout>
