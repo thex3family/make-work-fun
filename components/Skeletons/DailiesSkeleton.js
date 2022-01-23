7bed9cbf0d6fc690a25b949b272a684d5e427df2
Conrad Lin <git@conradlin.com>
2022-01-23 11:04:09 +0800

    Fix skeleton for dailies mobile

--- components/Skeletons/DailiesSkeleton.js
+++ components/Skeletons/DailiesSkeleton.js
@@ -8,7 +8,7 @@ export default function DailiesSkeleton() {
         <div className="max-w-6xl mx-auto py-8 md:pt-24 px-4 sm:px-6 lg:px-8 my-auto w-full flex flex-col">
           <div className="animate-fade-in-up rounded sm:bg-black sm:bg-opacity-90 bg-none bg-opacity-100 opacity-95">
             <div className="pt-10 pb-5">
-              <div className="mx-auto max-w-lg w-96 h-12 sm:h-20 bg-gray-600 rounded animate-pulse mb-6" />
+              <div className="mx-auto max-w-lg w-full sm:w-96 h-12 sm:h-20 bg-gray-600 rounded animate-pulse mb-6" />
               <div className="text-center mb-5">
                 <div className="w-24 h-24 border-4 border-primary-2 bg-gray-600 shadow-lg text-center inline-flex items-center justify-center mx-auto text-black my-2 font-semibold uppercase rounded-full text-4xl"></div>
                 <div>
@@ -19,7 +19,7 @@ export default function DailiesSkeleton() {
                     <i className="text-gray-800 far fa-star" />
                   </div>
                 </div>
-                <div className="mx-auto max-w-xs w-72 h-14 sm:h-16 mt-6 bg-gray-600 rounded animate-pulse mb-6" />
+                <div className="mx-auto max-w-xs w-full sm:w-72 h-14 sm:h-16 mt-6 bg-gray-600 rounded animate-pulse mb-6" />
               </div>
             </div>
             <div className="animate-fade-in-up mx-0">
