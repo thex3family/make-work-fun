1deb52bc0d80ba815cbc60fa0dfae4f567ca805a
Conrad Lin <git@conradlin.com>
2022-01-24 16:03:16 +0800

    Make close button not appear in mobile

--- components/Modals/ModalPomo.js
+++ components/Modals/ModalPomo.js
@@ -13,15 +13,16 @@ export default function ModalPomo({ visibility, setVisibility, mobileDevice }) {
           scale={1}>
           <div className={`${mobileDevice ? 'opacity-100' : 'opacity-20'} hover:opacity-100 inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl sm:my-8 sm:align-middle ${size == 'large' ? 'w-1/2' : size == 'medium' ? 'w-1/3' : 'w-full md:w-1/4'}`}>
 
-            <div className="handle bg-gray-50 px-4 py-3 sm:px-4 flex flex-row-reverse cursor-move">
-              <button onClick={() => setVisibility(false)} type="button" className="w-auto inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-                Close
-              </button>
+            <div className="handle bg-gray-50 px-4 py-3 flex flex-row-reverse cursor-move">
               <div className={`${mobileDevice ? 'hidden' : ''}`}>
               <button type="button" onClick={() => setSize('small')} className="fas fa-mobile-alt mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" />
               <button type="button" onClick={() => setSize('medium')} className="fas fa-tablet-alt mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" />
                <button type="button" onClick={() => setSize('large')} className="fas fa-laptop mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" />
-                 </div>
+                
+              <button onClick={() => setVisibility(false)} type="button" className="inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 ml-3 w-auto text-sm">
+                Close
+              </button>
+               </div>
             </div>
             <iframe
               className="w-full"
