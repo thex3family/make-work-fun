7758c03cd05d4bfa703e6cc761623e3bd59c6144
Conrad Lin <git@conradlin.com>
2022-04-07 05:37:47 +0800

    Improved design

--- components/ui/Footer/Footer.js
+++ components/ui/Footer/Footer.js
@@ -1,16 +1,17 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-8xl px-6 bg-primary-1">
+    <footer className="mx-auto max-w-8xl px-6 py-10 bg-primary-1">
       <div className="py-4 space-y-4 bg-primary-1">
         <div className="text-center">
           <span className="text-primary align-middle font-semibold">Version 0.39</span>
-          <a href="https://co-x3.com/?utm_source=makeworkfun" aria-label="Co-x3 Family">
+          <a href="https://co-x3.com/?utm_source=makeworkfun" target="_blank" aria-label="Co-x3 Family">
             <img
               src="/img/co-x3logo_white_full.png"
               alt="Co-x3 Family"
               className="inline-block h-6 ml-4 text-primary"
             />
           </a>
+          <p className="mt-4 text-sm text-primary align-middle font-semi-bold">Created with ♥ by the <a href="https://co-x3.com/?utm_source=makeworkfun" target="_blank" className="text-emerald-600 font-semibold">Co-x3 Family</a></p>
           <p className="mt-1 text-sm text-primary align-middle font-semi-bold">© All Rights Reserved</p>
         </div>
       </div>
