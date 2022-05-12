6eed94e09212cf2c948afdc048468dbf16c565fd
Conrad Lin <git@conradlin.com>
2022-05-12 23:00:06 +0800

    Disclaimer Documentation

--- components/ui/Footer/Footer.js
+++ components/ui/Footer/Footer.js
@@ -1,6 +1,8 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-8xl px-6 py-10 bg-primary-1">
+    <footer className="mx-auto max-w-8xl px-6 py-10 pb-32 bg-primary-1">
       <div className="py-4 space-y-4 bg-primary-1">
         <div className="text-center">
           <span className="text-primary align-middle font-semibold">Version 0.40</span>
@@ -11,8 +13,9 @@ export default function Footer() {
               className="inline-block h-6 ml-4 text-primary"
             />
           </a>
-          <p className="mt-4 text-sm text-primary align-middle font-semi-bold">Created with ♥ by the <a href="https://co-x3.com/?utm_source=makeworkfun" target="_blank" className="">Co-x3 Family</a></p>
-          <p className="mt-1 text-sm text-primary align-middle font-semi-bold">© All Rights Reserved</p>
+          <p className="mt-4 text-sm text-primary align-middle">Created with ♥ by the <a href="https://co-x3.com/?utm_source=makeworkfun" target="_blank" className="">Co-x3 Family</a></p>
+          <p className="mt-1 text-sm text-primary align-middle">© All Rights Reserved</p>
+          <p className="mt-4 text-sm text-emerald-500 align-middle font-semibold"><Link href="/docs/privacy-policy">Privacy Policy</Link> | <Link href="/docs/terms-of-use">Terms Of Use</Link></p>
         </div>
       </div>
     </footer>
