f5eb0001b6a1190ab9c8e9af0db2e7ce4dbc4564
Conrad Lin <git@conradlin.com>
2022-05-17 10:55:38 +0800

    Set loading state

--- pages/auth/notion/callback.js
+++ pages/auth/notion/callback.js
@@ -1,14 +1,17 @@
 import LoadingDots from '@/components/ui/LoadingDots';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Input from '@/components/ui/Input';
 import Button from '@/components/ui/Button';
 import { supabase } from '@/utils/supabase-client';
 
 export default function notionCallback({ response, user }) {
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         if (response.access_token) {
             updateDatabase()
+        } else {
+            setLoading(false);
         }
     }, [response])
 
@@ -23,7 +26,7 @@ export default function notionCallback({ response, user }) {
                 throw error;
             }
         } catch (error) {
-            alert(error.message);
+            console.log(error.message);
         } finally {
             updateNotionCredentials();
         }
@@ -44,35 +47,46 @@ export default function notionCallback({ response, user }) {
             alert(error.message);
         } finally {
             updateNotionCredentials();
+            setLoading(false)
         }
     }
 
-    return (
-        <>
-            <section className="justify-center align-center flex h-screen">
-                <div className="animate-fade-in-up max-w-6xl mx-auto py-8 md:pt-24 px-4 sm:px-6 lg:px-8 my-auto w-full flex flex-col">
-                    <div className="pb-10">
-                        <h1 className="text-4xl font-extrabold text-center sm:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-blue-500 pb-5">
-                            Connect With Notion
-                        </h1>
-                        <p className="text-xl text-accents-6 text-center sm:text-2xl max-w-2xl m-auto mt-4">
-                            {response.error ? <><span className='text-red-600 font-semibold bg-red-200 rounded px-2 py-0.5 w-auto'>Error <i className='fas fa-times align-middle' /></span> <p className='mt-2 text-accents-4'>Message: {response.error}</p><p className='mt-4'>Please try to connect again. If you experience this error repeatedly, please <a className="launch_intercom cursor-pointer font-semibold text-emerald-500">contact us.</a></p></> :
-                                <><span className='text-emerald-600 font-semibold bg-emerald-200 rounded px-2 py-0.5 w-auto'>Connected <i className='fas fa-check align-middle' /></span> <p className='mt-2 text-accents-4'>Workspace: {response.workspace_name}</p><p className='mt-4'>To set up your databases, go back to the account page.</p></>}
-                        </p>
+    if (!loading) {
+        return (
+            <>
+                <section className="justify-center align-center flex h-screen">
+                    <div className="animate-fade-in-up max-w-6xl mx-auto py-8 md:pt-24 px-4 sm:px-6 lg:px-8 my-auto w-full flex flex-col">
+                        <div className="pb-10">
+                            <h1 className="text-4xl font-extrabold text-center sm:text-6xl bg-clip-text text-transparent bg-gradient-to-r from-emerald-500 to-blue-500 pb-5">
+                                Connect With Notion
+                            </h1>
+                            <p className="text-xl text-accents-6 text-center sm:text-2xl max-w-2xl m-auto mt-4">
+                                {response.error ? <><span className='text-red-600 font-semibold bg-red-200 rounded px-2 py-0.5 w-auto'>Error <i className='fas fa-times align-middle' /></span> <p className='mt-2 text-accents-4'>Message: {response.error}</p><p className='mt-4'>Please try to connect again. If you experience this error repeatedly, please <a className="launch_intercom cursor-pointer font-semibold text-emerald-500">contact us.</a></p></> :
+                                    <><span className='text-emerald-600 font-semibold bg-emerald-200 rounded px-2 py-0.5 w-auto'>Connected <i className='fas fa-check align-middle' /></span> <p className='mt-2 text-accents-4'>Workspace: {response.workspace_name}</p><p className='mt-4'>To set up your databases, go back to the account page.</p></>}
+                            </p>
+                        </div>
+                        <div className="text-center mx-auto">
+                            <a href="/account?tab=connect&via=notion">
+                                <Button className="w-auto mx-auto mt-3"
+                                    variant="prominent"
+                                >
+                                    {response.error ? 'Try Again' : 'Continue'}
+                                </Button>
+                            </a>
+                        </div>
                     </div>
-                    <div className="text-center mx-auto">
-                        <a href="/account?tab=connect&via=notion">
-                            <Button className="w-auto mx-auto mt-3"
-                                variant="prominent"
-                            >
-                                {response.error ? 'Try Again' : 'Continue'}
-                            </Button>
-                        </a>
-                    </div>
-                </div>
-            </section>
-        </>
+                </section>
+            </>
+        );
+    }
+
+    
+    return(
+        <section className="justify-center h-screen flex">
+        <LoadingDots />
+      </section>
     );
+    
 }
 
 export async function getServerSideProps(context) {
