9ef4334fdf7cccafb832a511d2292e0da52ece0e
Conrad Lin <git@conradlin.com>
2022-05-24 16:13:59 +0800

    Hide banner on click

--- components/ui/ItemBanner.js
+++ components/ui/ItemBanner.js
@@ -21,6 +21,7 @@ const calculateTimeLeft = (expiry_time) => {
 const ItemBanner = ({ activeTimeItem }) => {
 
     const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(activeTimeItem.expiry_time));
+    const [show, setShow] = useState(true);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -45,21 +46,24 @@ const ItemBanner = ({ activeTimeItem }) => {
     });
 
 
-    return (
-        <div class="bg-yellow-300 border-b-2 border-dark">
+    if (show) return (
+        <div class={`${timerComponents.length ? 'bg-yellow-300 text-yellow-800' : 'bg-red-600 text-white'} border-b-2 border-dark`}>
             <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
                 <div class="flex items-center justify-center">
                     <div class="flex items-center py-1.5">
                         {/* <i class="flex p-2 rounded-lg bg-yellow-400 text-xl fas fa-coins text-yellow-800">
                         </i> */}
-                        <div class="ml-3 font-medium text-yellow-800 flex flex-col sm:flex-row items-center gap-1">
+                        <div class="ml-3 font-medium flex flex-col sm:flex-row items-center gap-1">
                             You've Purchased An Item! Finish <div className="flex flex-col sm:flex-row gap-1 items-center"><div className="bg-dark bg-opacity-80 px-2 py-1 text-white rounded">{activeTimeItem.item.name}</div> In <div className="bg-dark bg-opacity-80 px-2 py-1 text-white rounded">{timerComponents.length ? timerComponents : <span>0 seconds</span>}</div></div>
                         </div>
                     </div>
+                    <i className='ml-4 border-2 p-2 rounded text-sm fas fa-check hover:bg-emerald-600 cursor-pointer' onClick={()=>setShow(false)}/>
                 </div>
             </div>
         </div>
     );
+    
+    return null;
 }
 
 
