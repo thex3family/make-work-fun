829c8ae52c46bbeb1a5dc504884333138d718ce7
Conrad Lin <git@conradlin.com>
2023-04-13 13:41:09 +0800

    Update circle map

--- pages/map.js
+++ pages/map.js
@@ -48,72 +48,79 @@ export default function map({ metaBase, setMeta }) {
         </div>
         <div>
           {benefitTab == 1 ?
-            <><div className="mt-6 grid gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2 animate-fade-in-up">
-            <CardMap
-              name="The Tavern"
-              img_url="img/the-tavern.jpg"
-              desc="The starting place for all adventurers - find like-minded friends."
-              emojis="💬"
-              url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/9f7b04a7-edb3-4916-a110-92891beda1ae/chat"
-              availability="all"
-            />
-            <CardMap
-              name="Great Hall"
-              img_url="img/great-hall.jpg"
-              desc="Hang out with comrades before embarking on your next big adventure."
-              emojis="🗣 💬"
-              url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/156d818e-5d78-4302-b68a-99961906eaeb/stream"
-              availability="family"
-            />
-            <CardMap
-              name="Make Work Fun"
-              img_url="img/family-connection.jpg"
-              desc="Talk about the makework.fun app, and make friends with other players!"
-              emojis="💬"
-              url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/8dd9fd6d-cd47-47e0-8a2c-3e7d87034d69/chat"
-              availability="all"
-            />
-            <CardMap
-              name="Share Wins"
-              img_url="img/share-wins.jpg"
-              desc="Start meaningful discussions on your recent wins and support each other."
-              emojis="💬"
-              url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/43bb8933-cd8a-4ec2-90c8-607338b60c38/chat"
-              availability="all"
-            />
-            <CardMap
-              name="Events Calendar"
-              img_url="img/get-stuff-done.jpg"
-              desc="RSVP for learning opportunities and co-create with the family."
-              emojis="📅"
-              url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/7d8b697f-85c4-491d-9e16-6ba618b44cf1/calendar"
-              availability="all"
-            />
-            <CardMap
-              name="Workspace Inspo"
-              img_url="img/workspace-inspo.jpg"
-              desc="Get inspired by others on how they set up their work and play stations."
-              emojis="🖼"
-              url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/250043b8-6077-4de8-a70d-cb1fae8c6416/media"
-              availability="family"
-            />
-          </div>
-          </>
+            <>
+              <iframe 
+                className="w-full rounded"
+                height="1000"
+                allowFullScreen="true"
+                src="https://our.x3.family?iframe=true">
+                </iframe>
+              {/* <div className="mt-6 grid gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2 animate-fade-in-up">
+                <CardMap
+                  name="The Tavern"
+                  img_url="img/the-tavern.jpg"
+                  desc="The starting place for all adventurers - find like-minded friends."
+                  emojis="💬"
+                  url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/9f7b04a7-edb3-4916-a110-92891beda1ae/chat"
+                  availability="all"
+                />
+                <CardMap
+                  name="Great Hall"
+                  img_url="img/great-hall.jpg"
+                  desc="Hang out with comrades before embarking on your next big adventure."
+                  emojis="🗣 💬"
+                  url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/156d818e-5d78-4302-b68a-99961906eaeb/stream"
+                  availability="family"
+                />
+                <CardMap
+                  name="Make Work Fun"
+                  img_url="img/family-connection.jpg"
+                  desc="Talk about the makework.fun app, and make friends with other players!"
+                  emojis="💬"
+                  url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/8dd9fd6d-cd47-47e0-8a2c-3e7d87034d69/chat"
+                  availability="all"
+                />
+                <CardMap
+                  name="Share Wins"
+                  img_url="img/share-wins.jpg"
+                  desc="Start meaningful discussions on your recent wins and support each other."
+                  emojis="💬"
+                  url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/43bb8933-cd8a-4ec2-90c8-607338b60c38/chat"
+                  availability="all"
+                />
+                <CardMap
+                  name="Events Calendar"
+                  img_url="img/get-stuff-done.jpg"
+                  desc="RSVP for learning opportunities and co-create with the family."
+                  emojis="📅"
+                  url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/7d8b697f-85c4-491d-9e16-6ba618b44cf1/calendar"
+                  availability="all"
+                />
+                <CardMap
+                  name="Workspace Inspo"
+                  img_url="img/workspace-inspo.jpg"
+                  desc="Get inspired by others on how they set up their work and play stations."
+                  emojis="🖼"
+                  url="https://www.guilded.gg/thex3family/groups/Gza4RWEd/channels/250043b8-6077-4de8-a70d-cb1fae8c6416/media"
+                  availability="family"
+                />
+              </div> */}
+            </>
             :
             <div className='relative flex flex-col justify-center'>
 
-            <video className="relative rounded-xl animate-fade-in-up" autoplay="" loop={true}><source src="/vid/gather-demo.mp4" />
-            </video>
-            <a href="https://app.gather.town/app/CxOkO4jP0Q4UY6rt/thex3family" target="_blank" className='mx-auto '>
-            <Button
-              className="w-auto my-4 animate-fade-in-up"
-              variant="prominent"
-            >
-              Visit Our Space
-            </Button>
-            </a>
+              <video className="relative rounded-xl animate-fade-in-up" autoplay="" loop={true}><source src="/vid/gather-demo.mp4" />
+              </video>
+              <a href="https://app.gather.town/app/CxOkO4jP0Q4UY6rt/thex3family" target="_blank" className='mx-auto '>
+                <Button
+                  className="w-auto my-4 animate-fade-in-up"
+                  variant="prominent"
+                >
+                  Visit Our Space
+                </Button>
+              </a>
             </div>
-            
+
           }
 
         </div>
