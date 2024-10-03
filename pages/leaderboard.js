2a54a733f9f454d95a6184c846a38d77c9037a41
Conrad Lin <admin@conradlin.com>
2024-10-03 11:32:53 +0900

    Season 14

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -212,10 +212,10 @@ export default function HomePage({metaBase, setMeta, refreshChildStats, setRefre
                 <div className="w-full md:w-3/5 py-6 text-center">
                   <div className="max-w-6xl w-full md:w-11/12 lg:w-full xl:w-11/12 ml-auto py-8 px-0 sm:px-6 lg:px-8 my-auto bg-black bg-opacity-50 rounded-lg">
                     <h1 className="text-2xl font-semibold sm:text-3xl">
-                      Season 13 Statistics
+                      Season 14 Statistics
                     </h1>
                     <p className="text-sm text-accents-4 font-semibold">
-                      <i className='fas fa-calendar mr-2'/>Jul 1 - Sep 30
+                      <i className='fas fa-calendar mr-2'/>Oct 1 - Dec 31
                     </p>
                     <h1 className="rounded-lg pt-5 w-11/12 lg:w-full mx-auto text-sm font-semibold text-center lg:text-xl">
                       <LeaderboardStatistics
@@ -308,7 +308,7 @@ export default function HomePage({metaBase, setMeta, refreshChildStats, setRefre
               href="#link1"
               role="tablist"
             >
-              Season 13
+              Season 14
               <span
                 className={
                   'text-xs text-white py-1.5 px-2 ml-2 top-0 text-center border-2 shadow-lg rounded-full font-bold ' +
